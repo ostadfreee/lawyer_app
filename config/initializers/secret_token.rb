45d4b9ad021007a343b21700fe98525b56c29b72
ostadfreee <ostadfreee@yahoo.com>
2013-08-28 15:51:54 +0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LawyerApp::Application.config.secret_key_base = '6d1c377a98447be87adf69213bbaf866621bd65ce416bf60d04a17e31ef48ca38c9d74bc58c7e3f2c269e0fbfc81a310fd750ecf0e5caae00db68fe3200ab9f0'
